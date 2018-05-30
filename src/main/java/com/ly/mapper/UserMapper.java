d3c741c6f6a7c325d4b00533196868f548fdc9a4
qwe <1551097004@qq.com>
2018-05-30 17:32:27 +0800

    增加user的id uuid 策略

--- src/main/java/com/ly/mapper/UserMapper.java
+++ src/main/java/com/ly/mapper/UserMapper.java
@@ -1,17 +1,38 @@
 package com.ly.mapper;
 
 import com.ly.model.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.mapping.StatementType;
 
 /**
  * @author qwe
  */
 public interface UserMapper {
+
+	/**
+	 * 插入一条新的user
+	 * @param name
+	 * @param password
+	 * @param phone
+	 * @return
+	 */
+	@SelectKey(statement="select replace(UUID(),'-','') as id", keyProperty="id", before=true, statementType=StatementType.STATEMENT,resultType=String.class)
+	@Insert("INSERT INTO T_USER(NAME, PASSWORD, PHONE,id) VALUES(#{name}, #{password}, #{phone},#{id})")
+	int insert(@Param("name") String name, @Param("password") String password, @Param("phone") String phone);
+
+	/**
+	 * 根据电话号码查询
+	 * @param phone
+	 * @return
+	 */
 	@Select("SELECT * FROM T_USER WHERE PHONE = #{phone}")
 	User findUserByPhone(@Param("phone") String phone);
 
-	@Insert("INSERT INTO T_USER(NAME, PASSWORD, PHONE) VALUES(#{name}, #{password}, #{phone})")
-	int insert(@Param("name") String name, @Param("password") String password, @Param("phone") String phone);
+	/**
+	 * 根据id删除
+	 * @param id
+	 * @return
+	 */
+	@Delete("delete FROM T_USER where id = #{id}")
+	int deleteById(@Param("id") String id);
 }
--- src/main/java/com/ly/model/User.java
+++ src/main/java/com/ly/model/User.java
@@ -1,7 +1,7 @@
 package com.ly.model;
 
 public class User {
-	private Integer id;
+	private String id;
 	private String name;
 	private String password;
 	private String phone;
@@ -30,12 +30,12 @@ public class User {
 		this.phone = phone;
 	}
 
-	public Integer getId() {
+	public String getId() {
 
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 }
